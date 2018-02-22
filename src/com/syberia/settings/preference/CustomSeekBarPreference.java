8563a017718ff89bae3f1922847a34a2c8d0289e
xyyx <xyyx@mail.ru>
2018-02-22 15:20:18 +0800

    CustomSeekBar: Fix disabling when create before: https://imgur.com/a/jtNip after: https://imgur.com/a/Q1rJH
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/syberia/settings/preference/CustomSeekBarPreference.java
+++ src/com/syberia/settings/preference/CustomSeekBarPreference.java
@@ -134,6 +134,8 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
 
         view.setDividerAllowedAbove(false);
         //view.setDividerAllowedBelow(false);
+
+        mSeekBar.setEnabled(isEnabled());
     }
 
     public void setMax(int max) {
