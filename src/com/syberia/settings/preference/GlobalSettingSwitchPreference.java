71dc8dd81fb2f19deb001ba1f7339ca90c316bdf
bigrushdog <randall.rushing@gmail.com>
2018-08-20 18:02:26 +0800

    HW Keys customization support [1/2]
    Based on DUI
    
    jhenrique09 edits: make it DUI independent
    
    Original commit message:
    
    Same robust action library as used on software navigation. Supports
    single tap, double tap, and long press. As a precautionary measure,
    single tap back and single tap home are fixed and can not be changed.
    Camera button actions are not supported at this time. We will bring
    in wake key support at a later time.
    
    Also includes:
    
    DUI: Initial checkin for Oreo [4/7]

--- src/com/syberia/settings/preference/GlobalSettingSwitchPreference.java
+++ src/com/syberia/settings/preference/GlobalSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.syberia.settings.preferences;
+package com.syberia.settings.preference;
 
 import android.content.Context;
 import android.provider.Settings;
