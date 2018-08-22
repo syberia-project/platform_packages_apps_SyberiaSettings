7cd4c2763ddde711dc7910067ada809cb2c14e82
alexxxdev <alexxxdev@yandex.ru>
2018-08-22 23:38:35 +0700

    Buttons: Add buttons brightness and button backlight timeout options

--- src/com/syberia/settings/fragments/ButtonsSettings.java
+++ src/com/syberia/settings/fragments/ButtonsSettings.java
@@ -19,8 +19,11 @@
 
 package com.syberia.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
@@ -31,16 +34,59 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class ButtonsSettings extends SettingsPreferenceFragment {
+import com.syberia.settings.preference.CustomSeekBarPreference;
+
+public class ButtonsSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
+
+	private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
+    private static final String KEY_BACKLIGHT_TIMEOUT = "backlight_timeout";
+
+    private static final String CATEGORY_HWKEY = "hardware_keys";
+    
+    private ListPreference mBacklightTimeout;
+    private CustomSeekBarPreference mButtonBrightness;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.buttons_settings);
+
+		mBacklightTimeout = (ListPreference) findPreference(KEY_BACKLIGHT_TIMEOUT);
+		mButtonBrightness = (CustomSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+		if (mBacklightTimeout != null) {
+			mBacklightTimeout.setOnPreferenceChangeListener(this);
+			int BacklightTimeout = Settings.System.getInt(getContentResolver(), Settings.System.BUTTON_BACKLIGHT_TIMEOUT, 5000);
+			mBacklightTimeout.setValue(Integer.toString(BacklightTimeout));
+			mBacklightTimeout.setSummary(mBacklightTimeout.getEntry());
+		}
+		if (mButtonBrightness != null) {
+			int ButtonBrightness = Settings.System.getInt(getContentResolver(),	Settings.System.BUTTON_BRIGHTNESS, 255);
+			mButtonBrightness.setValue(ButtonBrightness / 1);
+			mButtonBrightness.setOnPreferenceChangeListener(this);
+		}
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SYBERIA;
+    }
+
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		ContentResolver resolver = getActivity().getContentResolver();
+
+		if (preference == mBacklightTimeout) {
+            String BacklightTimeout = (String) newValue;
+            int BacklightTimeoutValue = Integer.parseInt(BacklightTimeout);
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.BUTTON_BACKLIGHT_TIMEOUT, BacklightTimeoutValue);
+            int BacklightTimeoutIndex = mBacklightTimeout.findIndexOfValue(BacklightTimeout);
+            mBacklightTimeout.setSummary(mBacklightTimeout.getEntries()[BacklightTimeoutIndex]);
+            return true;
+        } else if (preference == mButtonBrightness) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.BUTTON_BRIGHTNESS, value * 1);
+            return true;
+        }
+        return false;
     }
 }
