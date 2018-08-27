360f208c6bad4b3d358de16543b15a20a7862b1a
Author: bigrushdog <randall.rushing@gmail.com>
2018-08-27 22:45:05 +0700

    Screen off animations [2/2]
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I6484fe91d54fdb8c09f612272006e8ab96b1a396
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- src/com/syberia/settings/fragments/GeneralTweaks.java
+++ src/com/syberia/settings/fragments/GeneralTweaks.java
@@ -16,28 +16,59 @@
 
 package com.syberia.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+
 import com.android.settings.R;
 
 import android.provider.SearchIndexableResource;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
-
 import com.android.settings.SettingsPreferenceFragment;
-
 import com.android.internal.logging.nano.MetricsProto;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class GeneralTweaks extends SettingsPreferenceFragment implements Indexable {
+public class GeneralTweaks extends SettingsPreferenceFragment implements Indexable, OnPreferenceChangeListener {
 
+	private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+	private ListPreference mScreenOffAnimation;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.general_tweaks);
+		ContentResolver resolver = getActivity().getContentResolver();
+        mScreenOffAnimation = (ListPreference) findPreference(SCREEN_OFF_ANIMATION);
+        int screenOffStyle = Settings.System.getInt(resolver, Settings.System.SCREEN_OFF_ANIMATION, 0);
+        mScreenOffAnimation.setValue(String.valueOf(screenOffStyle));
+        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
+        mScreenOffAnimation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		ContentResolver resolver = getActivity().getContentResolver();
+		if (preference == mScreenOffAnimation) {
+			String value = (String) newValue;
+			Settings.System.putInt(resolver,
+			Settings.System.SCREEN_OFF_ANIMATION, Integer.valueOf(value));
+			int valueIndex = mScreenOffAnimation.findIndexOfValue(value);
+			mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]);
+			return true;
+		}        
+    	return false;
     }
 
     @Override
