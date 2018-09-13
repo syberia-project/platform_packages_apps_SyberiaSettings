29ec414fc6ae9c0be0d7866693129f2e228d36f3
Danny Baumann <dannybaumann@web.de>
2018-09-13 20:59:16 +0700

    Settings: Add back increasing ring feature (2/3)
    This is a squash of the following commits:
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Tue Nov 11 13:14:07 2014 +0100
    
        Add back increasing ring feature (2/3).
    
        Change-Id: Iafc79a57651db327a100e0f1723b62f442b638be
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Mon Nov 24 13:05:39 2014 +0100
    
        Fix NPE.
    
        When entering the notification prefs sub activity without the increasing
        ring pref being visible and leaving it right away, onActivityStop()
        threw an NPE because the handler wasn't yet constructed.
    
        Change-Id: Ia4291d8ac3d1dbf76d432db7e0e973844163c4ae
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Fri May 29 08:48:46 2015 +0200
    
        Fix up increasing ring tone volume preview behaviour.
    
        Change-Id: Ibe7fdf1ee7f0aa1c7c76052df69fa92e8b6a2ffd
    
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Tue Jul 12 09:45:45 2016 -0700
    
        Settings: Always set ramp-up time value during bindView
    
        onProgressChange is not always called if we set the
        ramp up time to 5 sec (0 in seekbar value, since it is
        the default value, no change).
        Causing the text showing the ramp up time text value not
        updated and display as empty when user enable/disable
        increasing ring volume with ramp up time set to 5 sec.
    
        FEIJ-1572
    
        Change-Id: Id32d6b51aab05e80be59bc1912c4ae7c5d114ac5
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Wed, 18 Jan 2017 15:55:13 +0800
    
        Move notif volume pref on top of increasing ring pref
    
        Change Iafc79a57651db327a100e0f1723b62f442b638be added two
        increasing ringtone-related preferences between the notification
        volume preference and the ring volume preference.
    
        However, the notification volume preference should be grouped
        together with the media, alarm, and ring volume preferences.
    
        Hence, move the notification volume preference back to where
        it should be.
    
        Change-Id: I138e6f78b6c231643d86746858e0cbdfcb0942eb
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Mon Dec 11 23:45:39 2017 +0000
    
        Adapt increasing ring feature to O-MR1 and new Settings UI
    
         * Make it depend on SettingsLib's version of Lifecycle and Mixin.
    
         * Create a custom style for the IncreasingRingtonePreference
           and match the new UI in Oreo Settings app.
    
        Change-Id: I25462159fa39f5cdaddb1898c71f536572e900a4
    
    Change-Id: I3f22c77504a5014f9dbd40909d760149143570fe
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/syberia/settings/fragments/SystemSoundSettings.java
+++ src/com/syberia/settings/fragments/SystemSoundSettings.java
@@ -19,7 +19,11 @@
 
 package com.syberia.settings.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
@@ -28,19 +32,105 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.Utils;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+import com.syberia.settings.preference.IncreasingRingVolumePreference;
+import com.syberia.settings.controller.IncreasingRingPreferenceController;
+import com.syberia.settings.controller.IncreasingRingVolumePreferenceController;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class SystemSoundSettings extends SettingsPreferenceFragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SystemSoundSettings extends DashboardFragment {
+	private static final String TAG = "SystemSoundSettings";
+
+	private final IncreasingRingVolumePreferenceCallback mIncreasingRingVolumeCallback = new IncreasingRingVolumePreferenceCallback();
+
+	private static final int SAMPLE_CUTOFF = 2000;
+	static final int STOP_SAMPLE = 1;
+
+	final Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STOP_SAMPLE:
+                    mIncreasingRingVolumeCallback.stopSample();
+                    break;
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        addPreferencesFromResource(R.xml.system_sound_settings);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SYBERIA;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.system_sound_settings;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this, getLifecycle());
+    }
+
+	@Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        IncreasingRingVolumePreferenceController irvpc =
+                use(IncreasingRingVolumePreferenceController.class);
+        irvpc.setCallback(mIncreasingRingVolumeCallback);
+        getLifecycle().addObserver(irvpc);
+    }
+
+	@Override
+    public void onPause() {
+        super.onPause();
+        mIncreasingRingVolumeCallback.stopSample();
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context, SystemSoundSettings fragment, Lifecycle lifecycle) {
+     	final List<AbstractPreferenceController> controllers = new ArrayList<>();
+
+		controllers.add(new IncreasingRingPreferenceController(context));
+        controllers.add(new IncreasingRingVolumePreferenceController(context));        
+
+        return controllers;
+    }
+
+    final class IncreasingRingVolumePreferenceCallback implements
+            IncreasingRingVolumePreference.Callback {
+        private IncreasingRingVolumePreference mPlayingPref;
+
+        @Override
+        public void onSampleStarting(IncreasingRingVolumePreference pref) {
+            mPlayingPref = pref;
+            mHandler.removeMessages(STOP_SAMPLE);
+            mHandler.sendEmptyMessageDelayed(STOP_SAMPLE, SAMPLE_CUTOFF);
+        }
+
+        public void stopSample() {
+            if (mPlayingPref != null) {
+                mPlayingPref.stopSample();
+                mPlayingPref = null;
+            }
+        }
+    };
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 }
