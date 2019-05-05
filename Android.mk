cbe1bd2351ab02c1d1d1a84f2d2eb312db58a31f
Lukáš Sýkora <sykora.lukas@outlook.com>
2019-05-05 20:35:06 +0200

    Delete Android.mk

--- Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-LOCAL_STATIC_ANDROID_LIBRARIES += \
-    android-support-constraint-layout \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-cardview \
-    
-LOCAL_STATIC_JAVA_LIBRARIES += glide
