DirtyUnicorns/android_vendor_support

Import Custom switch and category
Layouts taken from our OnePlus extras repo
https://github.com/DirtyUnicorns/android_device_oneplus_extras/commit/8dea4ae33f201947b5f59440aa21c3a57dadd3f0

Change-Id: I95368d5b9ac0c1764dc977b9c049c6bfd4b2c24c

src/com/dirtyunicorns/support/preferences/CustomPreferenceCategory.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceCategory;
+
+import com.dirtyunicorns.support.R;
+
+public class CustomPreferenceCategory extends PreferenceCategory {
+
+    public CustomPreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public CustomPreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}

src/com/dirtyunicorns/support/preferences/CustomSwitchPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.SwitchPreference;
+
+import com.dirtyunicorns.support.R;
+
+public class CustomSwitchPreference extends SwitchPreference {
+
+    public CustomSwitchPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    public CustomSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+}

syberia-project/platform_packages_apps_SyberiaSettings

Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/syberia/settings/preference/CustomPreferenceCategory.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceCategory;
+
+import com.android.settings.R;
+
+public class CustomPreferenceCategory extends PreferenceCategory {
+
+    public CustomPreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public CustomPreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}

src/com/syberia/settings/preference/CustomSwitchPreference.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+
+public class CustomSwitchPreference extends SwitchPreference {
+
+    public CustomSwitchPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+
+    public CustomSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_material_settings);
+    }
+}