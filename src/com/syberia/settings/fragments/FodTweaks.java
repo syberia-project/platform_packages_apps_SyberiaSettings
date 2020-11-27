ff2f6c2b3331d275e991225cd8f28751cde79d2b
kondors1995 <normandija1945@gmail.com>
2020-11-27 16:13:24 +0000

    Squashed: FOD icon picker [2/2]
    Squashed commit of the following:
    
    commit c6f411938f422b9eaa9ba8ac6d78afe52155346e
    Author: kondors1995 <normandija1945@gmail.com>
    Date:   Fri Nov 27 12:40:12 2020 +0000
    
        convert to webp
    
        Change-Id: Ie2df0039158d5ed5cb9c5d7fe0045e2b74e0a500
    
    commit 9425822e3c0da0255eeed94f9d691d7ae416201e
    Author: AnierinB <anierinb@evolution-x.org>
    Date:   Mon Feb 17 23:27:41 2020 +0000
    
        Evolution: Add fire_ice_ouroboros style FOD icon [2/2]
    
        Signed-off-by: AnierinB <anierinb@evolution-x.org>
    
    commit e7e08fa488d54c9fe2a604d0bfa562483a12e5a0
    Author: AnierinB <anierinb@evolution-x.org>
    Date:   Sun Feb 9 02:30:43 2020 +0000
    
        Evolution: Add scratch style FOD icons [2/2]
    
        Co-authored-by: deadlyghost088 <raag.naidu@gmail.com>
        Co-authored-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: AnierinB <anierinb@evolution-x.org>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    commit f1f6116c9f003738fb92c2669d30f7ba3936ab6d
    Author: Joey Huab <team.evolutionx.rom@gmail.com>
    Date:   Wed Jan 8 14:43:37 2020 +0800
    
        Evolution: Add Evo FOD icons [2/2]
    
        Co-authored-by: ecnivtwelve <ecnivtwelve@users.noreply.github.com>
        Co-authored-by: MartonDev <martondeveloper@gmail.com>
        Change-Id: I8b68b0e00149eae604694e7f3341616ccdaf6a14
    
    commit de935762ae0040c624b30843e1554ebd8a47a926
    Author: Stallix <team.evolutionx.rom@gmail.com>
    Date:   Fri Jan 3 16:46:45 2020 +0000
    
        syberia: Add more FOD icons [2/2]
    
        * Improve and re-write some codes
        * Use 3 columns for choosing the icons
    
        Change-Id: I43e48e8d0878bc9f5846ff81f4ad24228581709a
    
    commit 1aed5e291cfa4736ce75f18fc939a407a6a5f4f5
    Author: 00day0 <therandomuser11@gmail.com>
    Date:   Mon Dec 16 21:46:12 2019 +0100
    
        FOD icon picker [2/2]
    
        * Add a footer to explain how users can add their own icons
    
        Co-authored-by: Stallix <thestallix@gmail.com>
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Change-Id: I38e1e79dc3bff68fef2f05cac23c622f413a96b8
        Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    Change-Id: I5af323860a3036b25a79befdc8f593f2f9184211

--- src/com/syberia/settings/fragments/FodTweaks.java
+++ src/com/syberia/settings/fragments/FodTweaks.java
@@ -18,7 +18,12 @@ package com.syberia.settings.fragments;
 
 import android.os.Bundle;
 import android.content.Context;
+import android.content.res.Resources;
 import com.android.settings.R;
+import android.provider.SearchIndexableResource;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -26,10 +31,20 @@ import com.android.internal.logging.nano.MetricsProto;
 
 public class FodTweaks extends SettingsPreferenceFragment {
 
+    private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker_category";
+    private Preference mFODIconPicker;
+
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.fod_tweaks);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mFODIconPicker = (Preference) findPreference(FOD_ICON_PICKER_CATEGORY);
+        if (mFODIconPicker != null
+                && !getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView)) {
+            prefScreen.removePreference(mFODIconPicker);
+        }
     }
 
     @Override
