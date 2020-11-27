cee66b09f241c42b9b030f1d9f99ba70179a5066
DennySPb <dennyspb@gmail.com>
2020-11-27 15:29:00 +0300

    Create FOD tweaks fragment
    and hide it on non-FOD devices
    
    Change-Id: Ic652bcadcaa89c1cee7bba8fdb15ccaff4ad06af
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/syberia/settings/fragments/SystemSettings.java
+++ src/com/syberia/settings/fragments/SystemSettings.java
@@ -23,6 +23,9 @@ import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference;
+
 import android.provider.SearchIndexableResource;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
@@ -38,13 +41,26 @@ import android.os.UserHandle;
 @SearchIndexable
 public class SystemSettings extends SettingsPreferenceFragment {
 
+    private static final String FOD_DISABLED_BY_PROP =
+                                "ro.fingerprint.inscreen_disabled";
+
+    private static final String FOD_TWEAKS = "fod_tweaks";
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.system_settings);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        if (!isFODdevice()) {
+            prefScreen.removePreference(findPreference(FOD_TWEAKS));
+        }
     }
 
+    private boolean isFODdevice() {
+        return (getResources().getBoolean(
+                com.android.internal.R.bool.config_needCustomFODView));
+    }
+    
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SYBERIA;
