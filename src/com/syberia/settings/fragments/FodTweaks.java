5d28573606df8a1866954f1f39d26500a79febf7
DennySPb <dennyspb@gmail.com>
2020-12-01 15:37:22 +0300

    FodTweaks: remove FOD animation preferences if no resource package installed
    Change-Id: I61fa95a8c7f71fb4545c3181b9e11e7a245a0a0c
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/syberia/settings/fragments/FodTweaks.java
+++ src/com/syberia/settings/fragments/FodTweaks.java
@@ -5,17 +5,38 @@
  * limitations under the License.
  */
 package com.syberia.settings.fragments;
+
 import android.os.Bundle;
 import android.content.Context;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+
+import com.syberia.settings.Utils;
+
 public class FodTweaks extends SettingsPreferenceFragment {
+
+    private static final String FOD_ANIMATION_CATEGORY = "fod_animations";
+
+    private Context mContext;
+
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         addPreferencesFromResource(R.xml.fod_tweaks);
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PreferenceCategory fodCat = (PreferenceCategory) prefScreen
+                .findPreference(FOD_ANIMATION_CATEGORY);
+        final boolean isFodAnimationResources = Utils.isPackageInstalled(getContext(),
+                      getResources().getString(com.android.internal.R.string.config_fodAnimationPackage));
+        if (!isFodAnimationResources) {
+            prefScreen.removePreference(fodCat);
+        }
     }
+
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SYBERIA;
