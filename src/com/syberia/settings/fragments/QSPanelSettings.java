5312327c3f0cff9aa41975ca8e8ec462ac077858
spkal01 <kalligeross@gmail.com>
2021-06-30 21:14:36 +0530

    SyberiaSettings: Introduce Brightness Slider Styles [2/2]
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I2fb63bc67b4dfa2fa6b63057a52df3a4c27535d0
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- src/com/syberia/settings/fragments/QSPanelSettings.java
+++ src/com/syberia/settings/fragments/QSPanelSettings.java
@@ -18,27 +18,145 @@ package com.syberia.settings.fragments;
 
 import android.provider.SearchIndexableResource;
 
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
 import android.content.Context;
+import android.content.om.IOverlayManager;
 import com.android.settings.R;
 
+import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.syberia.settings.preference.SystemSettingListPreference;
+
 @SearchIndexable
-public class QSPanelSettings extends SettingsPreferenceFragment {
+public class QSPanelSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+	private static final String SLIDER_STYLE  = "slider_style";
+
+	private Handler mHandler;
+	private IOverlayManager mOverlayManager;
+    private IOverlayManager mOverlayService;
+
+	private SystemSettingListPreference mSlider;
 
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.qs_panel_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        mSlider = (SystemSettingListPreference) findPreference(SLIDER_STYLE);
+        mCustomSettingsObserver.observe();
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SLIDER_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SLIDER_STYLE))) {
+                updateSlider();
+            }
+        }
+    }
+
+    private void updateSlider() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean sliderDefault = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 0;
+        boolean sliderOOS = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 1;
+        boolean sliderAosp = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 2;
+        boolean sliderRUI = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 3;
+
+        if (sliderDefault) {
+            setDefaultSlider(mOverlayService);
+        } else if (sliderOOS) {
+            enableSlider(mOverlayService, "com.android.theme.systemui_slider_oos");
+        } else if (sliderAosp) {
+            enableSlider(mOverlayService, "com.android.theme.systemui_slider.aosp");
+        } else if (sliderRUI) {
+            enableSlider(mOverlayService, "com.android.theme.systemui_slider.rui");
+        }
+    }
+
+    public static void setDefaultSlider(IOverlayManager overlayManager) {
+        for (int i = 0; i < SLIDERS.length; i++) {
+            String sliders = SLIDERS[i];
+            try {
+                overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void enableSlider(IOverlayManager overlayManager, String overlayName) {
+        try {
+            for (int i = 0; i < SLIDERS.length; i++) {
+                String sliders = SLIDERS[i];
+                try {
+                    overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String[] SLIDERS = {
+        "com.android.theme.systemui_slider_oos",
+        "com.android.theme.systemui_slider.aosp",
+        "com.android.theme.systemui_slider.rui"
+    };
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		if (preference == mSlider) {
+		mCustomSettingsObserver.observe();
+        return true;
+        }
+        return false;
     }
 
     @Override
