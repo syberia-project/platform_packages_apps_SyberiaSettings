bdf03f413083ce8959ef3d2572a6acbe983953a1
spkal01 <kalligeross@gmail.com>
2021-07-11 16:15:23 +0530

    SyberiaSettings: Introduce A12 Brightness Slider [2/2]
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I8dd1eb2710afc3dd197e2da99f4bf47e178d7e6b
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- src/com/syberia/settings/fragments/QSPanelSettings.java
+++ src/com/syberia/settings/fragments/QSPanelSettings.java
@@ -112,6 +112,8 @@ public class QSPanelSettings extends SettingsPreferenceFragment implements OnPre
                 Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 2;
         boolean sliderRUI = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 3;
+        boolean sliderA12 = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 4;
 
         if (sliderDefault) {
             setDefaultSlider(mOverlayService);
@@ -121,6 +123,8 @@ public class QSPanelSettings extends SettingsPreferenceFragment implements OnPre
             enableSlider(mOverlayService, "com.android.theme.systemui_slider.aosp");
         } else if (sliderRUI) {
             enableSlider(mOverlayService, "com.android.theme.systemui_slider.rui");
+        } else if (sliderA12) {
+            enableSlider(mOverlayService, "com.android.theme.systemui_slider.a12");
         }
     }
 
@@ -154,7 +158,8 @@ public class QSPanelSettings extends SettingsPreferenceFragment implements OnPre
     public static final String[] SLIDERS = {
         "com.android.theme.systemui_slider_oos",
         "com.android.theme.systemui_slider.aosp",
-        "com.android.theme.systemui_slider.rui"
+        "com.android.theme.systemui_slider.rui",
+        "com.android.theme.systemui_slider.a12"
     };
 
     @Override
